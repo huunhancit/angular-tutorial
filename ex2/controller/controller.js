61e8921b8011b6f64ad99cd10480bcf496ecf1b5
dhnhan <huunhancit@gmail.com>
2015-09-16 17:28:33 +0700

    add ex2

--- ex2/controller/controller.js
+++ ex2/controller/controller.js
@@ -1,5 +1,15 @@
 var app = angular.module('App3',[]);
 app
-	.controller('App3Ctr',function(){
-		
+	.controller('App3Ctr',function($scope){
+		$scope.data = {};
+		$scope.data.clickButton = function(){
+			alert('Hello Nhan Dinh');
+		}
+		$scope.click2 = function(event){
+			alert('Clicked:' + event.clientX + ', ' + event.clientY);
+		};
+		$scope.data.versions = [{nameVersion: 1},{nameVersion: 2},{nameVersion: 3},{nameVersion: 4},{nameVersion: 5}];
+		$scope.click3 = function(version,event){
+			alert('Version: '+ version.nameVersion);
+		}
 	});
